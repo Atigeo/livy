d64eb91218b5f53ae4fd91c503dba77561c88c1a
Ioan Cristian Schuszter <cristian.schuszter@atigeo.com>
2016-05-31 14:08:29 +0300

    Minor mods to defaults

--- start-livy-debug.sh
+++ start-livy-debug.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 env \
-  LIVY_SERVER_JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Dlivy.server.session.factory=yarn -Djava.security.krb5.realm=STAGING.XPATTERNS.COM -Djava.security.krb5.kdc=10.0.2.181" \
+  LIVY_SERVER_JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Djava.security.krb5.realm=STAGING.XPATTERNS.COM -Djava.security.krb5.kdc=10.0.2.122" \
   ./bin/livy-server
